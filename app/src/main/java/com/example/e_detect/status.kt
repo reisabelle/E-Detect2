ba5221bc6f6997674d665d6445cc7af51967b79d
Reina Isabelle Abejuro <166028125+reisabelle@users.noreply.github.com>
2024-09-21 10:37:18 +0800

    Update status.kt

--- app/src/main/java/com/example/e_detect/status.kt
+++ app/src/main/java/com/example/e_detect/status.kt
@@ -1,14 +1,21 @@
 package com.example.e_detect
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class status : AppCompatActivity() {
+    private lateinit var backbtn: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_status)
+
+        backbtn = findViewById(R.id.backbtn)
+
+        backbtn.setOnClickListener{
+            val intent = Intent(this, StartingUi::class.java)
+            startActivity(intent)
+            }
     }
 }
